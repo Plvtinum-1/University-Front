462e9cfed0dfa2575deba20ce631c712e3998194
Ismail <dahriiismail@gmail.com>
2022-02-16 23:52:39 +0100

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route, Link, createRoutesFromChildren } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import down from './assets/down.svg'
 import './App.scss'
 
