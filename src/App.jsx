9bc4669ea66896d7c6489f4c9a2f28fa47216433
Ismail <dahriiismail@gmail.com>
2022-02-16 23:48:22 +0100

    additional changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route, Link, createRoutesFromChildren } from 'react-router-dom'
-import './App.css'
-
-import image from './assets/one.jpg'
-import image2 from './assets/2.jpg'
+import down from './assets/down.svg'
+import './App.scss'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="/course" element={<Coursepage />} />
+        <Route path="/math-course" element={<MathCourse />} />
+        <Route path="/programming-course" element={<ProgrammingCourse />} />
       </Routes>
     </BrowserRouter>
   ); 
@@ -18,33 +17,79 @@ function App() {
 
 export function Homepage() {
   return (
-    <div className="container">
+    <div className='container'>
       <div className='wrapper'>
-        <div className='image-container'>
-          <img src={image} />
-          <Link to={'/course'}>Show details</Link>
-          <div className='overlay'>
-            <h1>hello</h1>
+        <div className="card">
+          <div className="content">
+            <h2 className="title">Math Course</h2>
+            <p className="copy">This course contains everything you need related to maths</p>
+            <button className="btn"><Link className='link' to={'/math-course'}>View Course</Link></button>
           </div>
-          <p>lorem impsum</p>
         </div>
-        <div className='image-container'>
-          <img src={image2} />
-          <Link to={'/course'}>Show details</Link>
-          <div className='overlay'>
-            <h1>hello</h1>
+        <div className="card2">
+          <div className="content">
+            <h2 className="title">Programming Course</h2>
+            <p className="copy">This course contains everything you need related to programming and algorithms</p>
+            <button className="btn"><Link className='link' to={'/programming-course'}>View Course</Link></button>
           </div>
-          <p>lorem impsum</p>
         </div>
       </div>
   </div>
   )
 }
 
-export function Coursepage() {
+function MathCourse() {
   return (
-    <div>
-      Video Player
+    <div className='container'>
+      <div className='wrapper'>
+        <div className='video'>
+         <div className='header'>
+           <h2 className='title'>Math Course</h2>
+           <Link to={'/'} className='link'>Back</Link>
+          </div>
+          <iframe src="https://www.youtube.com/embed/7Xn7pTBFTq0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <h3 className='title'>Description</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde alias debitis dignissimos quis, aut ducimus laboriosam quibusdam. Mollitia enim, ea animi assumenda quod placeat illo dicta nam, quidem ipsum repellendus.</p>
+        </div>
+        <div className='sections'>
+          <h2 className='title'>Course Overview</h2>
+          <div className='section'>
+            <div className='inside-section'>
+              <h4>Section 1 : Course Introduction</h4>
+              <img className='image' src={down} alt="" />
+            </div>
+            <span>31 Min</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+function ProgrammingCourse() {
+  return (
+    <div className='container'>
+      <div className='wrapper'>
+        <div className='video'>
+          <div className='header'>
+           <h2 className='title'>Programming Course</h2>
+           <Link to={'/'} className='link'>Back</Link>
+          </div>
+          <iframe src="https://www.youtube.com/embed/1LrXsP4xSEc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <h3 className='title'>Description</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde alias debitis dignissimos quis, aut ducimus laboriosam quibusdam. Mollitia enim, ea animi assumenda quod placeat illo dicta nam, quidem ipsum repellendus.</p>
+        </div>
+        <div className='sections'>
+          <h2 className='title'>Course Overview</h2>
+          <div className='section'>
+            <div className='inside-section'>
+              <h4>Section 1 : Course Introduction</h4>
+              <img className='image' src={down} alt="" />
+            </div>
+            <span>31 Min</span>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
